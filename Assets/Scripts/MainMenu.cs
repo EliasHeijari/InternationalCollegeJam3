340eef9118703096eecec949a99521d163849d5e
Elias Heijari <69460338+elkkuzq@users.noreply.github.com>
2023-12-14 18:23:01 +0200

    MainMenu Update

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,16 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] private Volume volume;
+    private Bloom bloom;
+
+    public float targetBloomIntensity = 200f;
+    public float transitionDuration = 2f;
+
+    private float initialBloomIntensity;
+    private float elapsedTime = 0f;
 
     public void PlayGame()
     {
-        // Load the "GameScene" when the "Play" button is pressed.
+        volume.profile.TryGet(out bloom);
+        // Store the initial bloom intensity
+        initialBloomIntensity = bloom.intensity.value;
+
+        // Start the coroutine to smoothly increase bloom intensity
+        StartCoroutine(IncreaseBloomOverTime());
+        StartCoroutine(PlayGameScene());
+    }
+    IEnumerator PlayGameScene()
+    {
+        yield return new WaitForSeconds(1.5f);
         SceneManager.LoadScene("GameScene");
+    }
+    IEnumerator IncreaseBloomOverTime()
+    {
+        while (elapsedTime < transitionDuration)
+        {
+            // Interpolate between initial and target bloom intensity
+            bloom.intensity.value = Mathf.Lerp(initialBloomIntensity, targetBloomIntensity, elapsedTime / transitionDuration);
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        // Ensure the target value is reached exactly
+        bloom.intensity.value = targetBloomIntensity;
     }
 
     // This method is called when the "Quit" button is pressed in the main menu.
